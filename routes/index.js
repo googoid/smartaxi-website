8b016fea0812d165c504ff9c60b847a26aeae6c1
David Kasparov <dave.kasparov@gmail.com>
2015-03-16 14:36:51 +0400

    arian amu

--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,31 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-  	lang_id: 'am',
-  	title: 'SmartTaxi',
-  	body_top_text: '',
-  	body_bottom_text: '',
-  	coming_soon_text: 'Շուտով բոլոր բջջային սարքավորումների համար'
+    lang_id: 'am',
+    title: 'SmartTaxi',
+    body_top_text: 'Ազգային արժեքների մասին խոսելը դարձել է նորաձև: Սովորաբար այդ խոսակցություններն արդյունք չեն հետևողական փնտրումների և սեղանային մտքի մակերեսային առկայծումներից անդին չեն անցնում: Այսուհանդերձ, մարդիկ ինչ-որ չափով, իրենց գիտակցության մի անկյունում, կարևորում են դրանց գոյությունը` համառորեն հավատալով, որ այդ արժեքները կան:',
+    body_bottom_text: 'Ազգային արժեքների մասին խոսելը դարձել է նորաձև',
+    coming_soon_text: 'Շուտով բոլոր բջջային սարքավորումների համար'
   });
 });
 
 router.get('/en', function(req, res, next) {
   res.render('index', { 
-  	lang_id: 'en',
-  	title: 'SmartTaxi',
-  	body_top_text: '',
-  	body_bottom_text: '',
-  	coming_soon_text: 'Coming soon on mobile devices'
+    lang_id: 'en',
+    title: 'SmartTaxi',
+    body_top_text: '',
+    body_bottom_text: '',
+    coming_soon_text: 'Coming soon on mobile devices'
   });
 });
 
 router.get('/ru', function(req, res, next) {
   res.render('index', { 
-  	lang_id: 'ru',
-  	title: 'SmartTaxi',
-  	body_top_text: '<strong>SmartTaxi</strong> – это новый, современный, быстрый, удобный и, самое главное, простой сервис для поиска и вызова такси на территории Армении посредством мобильных технологий. Качественное, надежное и честное обслуживание клиентов, минимальная обратная связь, и множество других полезных возможностей.',
-  	body_bottom_text: '<span class="strong">SmartTaxi</span> – умные технологии для умных людей.',
-  	coming_soon_text: 'Скоро на мобильных устройствах'
+    lang_id: 'ru',
+    title: 'SmartTaxi',
+    body_top_text: '<strong>SmartTaxi</strong> – это новый, современный, быстрый, удобный и, самое главное, простой сервис для поиска и вызова такси на территории Армении посредством мобильных технологий. Качественное, надежное и честное обслуживание клиентов, минимальная обратная связь, и множество других полезных возможностей.',
+    body_bottom_text: '<span class="strong">SmartTaxi</span> – умные технологии для умных людей.',
+    coming_soon_text: 'Скоро на мобильных устройствах'
   });
 });
 
