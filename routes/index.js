ceb2edf9e3c41c5d33470264223324ec9a9e6baa
David Kasparov <dave.kasparov@gmail.com>
2015-03-16 17:45:49 +0400

    text

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { 
     lang_id: 'am',
     title: 'SmartTaxi',
-    body_top_text: 'Ազգային արժեքների մասին խոսելը դարձել է նորաձև: Սովորաբար այդ խոսակցություններն արդյունք չեն հետևողական փնտրումների և սեղանային մտքի մակերեսային առկայծումներից անդին չեն անցնում: Այսուհանդերձ, մարդիկ ինչ-որ չափով, իրենց գիտակցության մի անկյունում, կարևորում են դրանց գոյությունը` համառորեն հավատալով, որ այդ արժեքները կան:',
-    body_bottom_text: 'Ազգային արժեքների մասին խոսելը դարձել է նորաձև',
+    body_top_text: 'SmartTaxi - ին նոր, ժամանակակից, արագ, հարմարավետ և պարզ միջոց է Հայաստանի տարածքում բջջային տեխնոլոգիաների միջոցով տաքսի փնտրելու և պատվիրելու համար։ Հաճախորդների` որակյալ, վստահելի և արդար սպասարկում, մինիմալ հետադարձ կապ և բազմաթիվ այլ օգտակար հնարավորություններ։',
+    body_bottom_text: 'SmartTaxi - խելացի տեխնոլոգիաներ՝ խելացի մարդկանց համար',
     coming_soon_text: 'Շուտով բոլոր բջջային սարքավորումների համար'
   });
 });
@@ -17,7 +17,7 @@ router.get('/en', function(req, res, next) {
     lang_id: 'en',
     title: 'SmartTaxi',
     body_top_text: 'The reason for their not coming to Yerevan is an atmosphere fear. They preferred nonparticipation to finding themselves persecuted on returning from Yerevan. I regret that after any program an organization that had had a meeting with its Armenian partners was charged with high treason. This atmosphere of fear is difficult to cope with, and people prefer not coming,” Mr Poghosyan said.',
-    body_bottom_text: 'An Armenian delegation participated in a Euronest',
+    body_bottom_text: 'SmartTaxi - smart technologies for smart people',
     coming_soon_text: 'Coming soon on mobile devices'
   });
 });
@@ -26,7 +26,7 @@ router.get('/ru', function(req, res, next) {
   res.render('index', { 
     lang_id: 'ru',
     title: 'SmartTaxi',
-    body_top_text: '<strong>SmartTaxi</strong> – это новый, современный, быстрый, удобный и простой сервис для поиска и вызова такси на территории Армении посредством мобильных технологий. Качественное, надежное и честное обслуживание клиентов, минимальная обратная связь и множество других полезных возможностей.',
+    body_top_text: '<strong>SmartTaxi</strong> – это новый, современный, быстрый, удобный и простой сервис для поиска и вызова такси на территории Армении посредством мобильных технологий. Качественное, надежное и честное обслуживание клиентов, связь с операторами лишь при необходимости и множество других полезных функций.',
     body_bottom_text: '<span class="strong">SmartTaxi</span> – умные технологии для умных людей.',
     coming_soon_text: 'Скоро на мобильных устройствах'
   });
