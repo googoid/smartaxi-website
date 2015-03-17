3703dd3aa2b99d26e9d0b176a66a5970859998fc
David Kasparov <dave.kasparov@gmail.com>
2015-03-17 18:46:23 +0400

    seo shit

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 jQuery( document ).ready(function( $ ) {
-	var imgs = ['./images/2k.jpg', './images/fhd.jpg', './images/logo.png', './images/btn_apple.png', './images/btn_google.png'];
+	var imgs = ['/images/2k.jpg', '/images/fhd.jpg', '/images/logo.png', '/images/btn_apple.png', '/images/btn_google.png'];
 
 	$('body').plainOverlay('show', {duration: 150, fillColor:'#251a36', opacity:'0.9', progress:function(){ return $('<div id="followingBallsG"><div id="followingBallsG_1" class="followingBallsG"></div><div id="followingBallsG_2" class="followingBallsG"></div><div id="followingBallsG_3" class="followingBallsG"></div><div id="followingBallsG_4" class="followingBallsG"></div>');} });
 
