ba6073cb75caa8dc76b64fc1ddc82e00687bfc7f
David Kasparov <dave.kasparov@gmail.com>
2015-03-17 18:25:12 +0400

    titles updated

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { 
     lang_id: 'am',
-    title: 'SmartTaxi',
+    title: 'SmartTaxi – Երևան',
     body_top_text: 'SmartTaxi՝  նոր, ժամանակակից, արագ, հարմարավետ և պարզ միջոց, Հայաստանի տարածքում բջջային տեխնոլոգիաների միջոցով տաքսի փնտրելու և պատվիրելու համար։ Որակյալ, վստահելի և արդար սպասարկում, օպերատորների հետ կապ բացառապես անհրաժեշտության դեպքում և բազում այլ հնարավորություններ:',
     body_bottom_text: 'SmartTaxi - խելացի տեխնոլոգիաներ՝ խելացի մարդկանց համար',
     coming_soon_text: 'Շուտով բոլոր բջջային սարքավորումների համար'
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
 router.get('/en', function(req, res, next) {
   res.render('index', { 
     lang_id: 'en',
-    title: 'SmartTaxi',
+    title: 'SmartTaxi – Yerevan',
     body_top_text: 'SmartTaxi - A completely modern, quick and convenient way of finding a cab in Armenia using your mobile device. Reliable and quality customer service, connection with the operators in case of necessity. Simple, yet powerful interface packed with myriads of useful features.',
     body_bottom_text: 'SmartTaxi - smart technologies for smart people',
     coming_soon_text: 'Coming soon on mobile devices'
@@ -25,7 +25,7 @@ router.get('/en', function(req, res, next) {
 router.get('/ru', function(req, res, next) {
   res.render('index', { 
     lang_id: 'ru',
-    title: 'SmartTaxi',
+    title: 'SmartTaxi - Ереван',
     body_top_text: '<strong>SmartTaxi</strong> – это новый, современный, быстрый, удобный и простой сервис для поиска и вызова такси на территории Армении посредством мобильных технологий. Качественное, надежное и честное обслуживание клиентов. Связь с операторами при исключительной необходимости. И множество других полезных функций.',
     body_bottom_text: '<span class="strong">SmartTaxi</span> – умные технологии для умных людей.',
     coming_soon_text: 'Скоро на мобильных устройствах'
